b174238d7368d5f5db28f811257d650a5761dd82
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2010-08-04 07:03:40 +0200

    Add stress testing. Fix few options for older Erlang. Add async add. Store Socket in process dictionary, as on Linux we can easly be run out of files.

--- simplememcache.erl
+++ simplememcache.erl
@@ -4,44 +4,56 @@
 % Copyright 2010, Witold Baryluk
 % Licencja BSD
 
+% for older ERTS:  remove write_concurrency, and [safe]
 
--export([start/0, start/1, server/1, process/5, loop/2]).
--export([add/2, get/1, add/3, get/2]).
+
+%-define(B2T(X), binary_to_term(X, [safe])).
+-define(B2T(X), binary_to_term(X)).
+-define(T2B(X), term_to_binary(X, [{minor_version,1}])).
+-define(T2B_CLIENT(X), term_to_binary(X, [compressed, {minor_version,1}])).
+
+-export([start/0, start/1, server/1, process/5, loop/3]).
+-export([add/2, get/1, add/3, get/2, async_add/3, async_add/2]).
 
 
 -define(PORT, 41728).
--define(IP, {127,0,0,1}).
+-define(IP, {10,0,2,5}).
+%-define(OtherOpts, [{write_concurrency, true}]).
+-define(OtherOpts, []).
 
 
 start() ->
 	start(?PORT).
 
 start(Port) ->
-	spawn(?MODULE, server, [Port]).
+	P = spawn(?MODULE, server, [Port]),
+%	timer:send_interval(1000, P, stat),
+	P.
 
 server(Port) ->
-	Tab = ets:new(?MODULE, [public, set, {write_concurrency, true}]),
-	{ok, Socket} = gen_udp:open(Port, [binary, {recbuf, 4096}]), % default is 8192
-	loop(Tab, Socket).
+	Tab = ets:new(?MODULE, [public, set, named_table] ++ ?OtherOpts),
+	{ok, Socket} = gen_udp:open(Port, [binary, {recbuf, 4096}, {reuseaddr, true}]), % default is 8192
+	loop(0, Tab, Socket).
 
-loop(Tab, Socket) ->
+loop(I, Tab, Socket) ->
 	receive
 		{udp, Socket, Src_IP, Src_Port, Packet} ->
 			spawn(?MODULE, process, [Tab, Socket, Src_IP, Src_Port, Packet]),
-			loop(Tab, Socket);
+			loop(I+1, Tab, Socket);
 		Other ->
-			io:format("Other message ~p~n", [Other]),
-			?MODULE:loop(Tab, Socket)
+			Info = ets:info(Tab),
+			io:format("Other message ~p. I=~p Info=~p~n", [Other, I, Info]),
+			?MODULE:loop(I, Tab, Socket)
 	end.
 
 process(Tab, Socket, Src_IP, Src_Port, Packet) ->
-	Data = case binary_to_term(Packet, [safe]) of
+	Data = case ?B2T(Packet) of
 		{add, Key, Value} ->
 			ets:insert(Tab, {Key, Value});
 		{get, Key} ->
 			ets:lookup(Tab, Key)
 	end,
-	BackPacket = term_to_binary(Data, [{compressed,3},{minor_version,1}]),
+	BackPacket = ?T2B(Data),
 	ok = gen_udp:send(Socket, Src_IP, Src_Port, BackPacket).
 
 
@@ -49,7 +61,27 @@ add(K, V) ->
 	add(?IP, K, V).
 
 add(IP, K, V) ->
-	client(IP, {add, K, term_to_binary(V,[{compressed,8},{minor_version,1}])}).
+	Data = {add, K, ?T2B_CLIENT(V)},
+	client(IP, Data).
+
+
+async_add(K, V) ->
+	async_add(?IP, K, V).
+
+async_add(IP, K, V) ->
+	Socket = case erlang:get(sim_socket) of
+		undefined ->
+			{ok, Socket1} = gen_udp:open(0, [binary, {recbuf, 4096}, {reuseaddr, true}]), % default is 8192
+			erlang:put(sim_socket, Socket1),
+			Socket1;
+		Socket1 -> Socket1
+	end,
+	Data = {add, K, ?T2B_CLIENT(V)},
+	Port = ?PORT,
+	Packet = ?T2B(Data),
+	ok = gen_udp:send(Socket, IP, Port, Packet).
+
+
 
 get(K) ->
 	get(?IP, K).
@@ -57,24 +89,33 @@ get(K) ->
 get(IP, K) ->
 	case client(IP, {get, K}) of
 		[{_,V}] ->
-			{value, binary_to_term(V, [safe])};
+			{value, ?B2T(V)};
 		[] ->
 			none
 	end.
 
 client(Op) ->
-	client({127,0,0,1},Op).
+	client(?IP,Op).
 
 client(IP, Op) ->
 	client(IP, ?PORT, Op).
 
 client(IP, Port, Data) ->
-	{ok, Socket} = gen_udp:open(0, [binary, {recbuf, 4096}]), % default is 8192
-	Packet = term_to_binary(Data, [{compressed,6}]),
+	Socket = case erlang:get(sim_socket) of
+		undefined ->
+			{ok, Socket1} = gen_udp:open(0, [binary, {recbuf, 4096}, {reuseaddr, true}]), % default is 8192
+			erlang:put(sim_socket, Socket1),
+			Socket1;
+		Socket1 -> Socket1
+	end,
+	client(Socket, IP, Port, Data).
+
+client(Socket, IP, Port, Data) ->
+	Packet = ?T2B(Data),
 	ok = gen_udp:send(Socket, IP, Port, Packet),
 	receive
 		{udp, Socket, IP, Port, BackPacket} ->
-			binary_to_term(BackPacket, [safe])
+			?B2T(BackPacket)
 		after 10000 ->
-			timeout
+			throw(timeout)
 	end.
---
+++ simplememcache_stress.erl
@@ -0,0 +1,37 @@
+-module(si).
+-export([go/0]).
+
+go() ->
+	MainSeed = erlang:md5(term_to_binary({self(),now(),make_ref()})),
+	<<A:32,B:32,C:32,_/binary>> = MainSeed,
+	random:seed(A, B, C),
+	loop(100000).
+
+loop(0) ->
+	done;
+loop(N) ->
+	%simplememcache:fullasync_add(key(), value()),
+	simplememcache:async_add(key(), value()),
+	flush(),
+	loop(N-1).
+
+
+key() ->
+	list_to_binary(
+		[$k,$a] ++ integer_to_list(random:uniform(100000)) ++ integer_to_list(random:uniform(100000)) ++ integer_to_list(random:uniform(100000))
+	).
+value() ->
+	lists:foldl(
+		fun(X,Acc) ->
+		E = {X,X, lists:seq(1, 5)},
+		[E|Acc]
+		end,
+		[],
+		lists:seq(1,5)
+	).
+
+flush() ->
+	receive
+		_ -> flush()
+		after 0 -> ok
+	end.
